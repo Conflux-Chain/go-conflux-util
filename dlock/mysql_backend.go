afd275c812726957a1def1b5c2cb5449dbaf0835
Conflux <wanliqunn@163.com>
2024-04-01 11:30:52 +0800

    improve `WriteEntry`

--- dlock/mysql_backend.go
+++ dlock/mysql_backend.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"time"
 
-	"github.com/go-sql-driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -20,9 +19,9 @@ type Dlock struct {
 	// The unique identifier for the lock.
 	Key string `gorm:"index:uidx_key,unique;not null"`
 	// The secret used to release the lock.
-	Nonce string `gorm:"default:not null"`
+	Nonce string `gorm:"not null"`
 	// When the lock expires.
-	ExpiredAt time.Time `gorm:"default:not null"`
+	ExpiredAt time.Time `gorm:"not null"`
 	// CRUD timestamp.
 	CreatedAt, UpdatedAt time.Time
 }
@@ -44,57 +43,38 @@ func NewMySQLBackend(db *gorm.DB) *MySQLBackend {
 
 // Implement WriteEntry - try to acquire a lock
 func (m *MySQLBackend) WriteEntry(
-	ctx context.Context, key, nonce string, lease time.Duration) (success bool, err error) {
-	if err = m.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
-		expAtGormExpr := gorm.Expr("NOW() + INTERVAL ? SECOND", lease.Seconds())
-		nowTsGormExpr := gorm.Expr("CURRENT_TIMESTAMP")
+	ctx context.Context, key, nonce string, lease time.Duration) (bool, error) {
+	ctxDb := m.db.WithContext(ctx)
+	expAtGormExpr := gorm.Expr("NOW() + INTERVAL ? SECOND", lease.Seconds())
+	nowTsGormExpr := gorm.Expr("CURRENT_TIMESTAMP")
 
-		// Attempt to insert a new lock
-		res := tx.Model(&Dlock{}).Create(map[string]interface{}{
+	err := ctxDb.First(&Dlock{}, "`key` = ?", key).Error
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, err
+	}
+
+	if err == nil { // Try to update if expired or nonce matches.
+		res := ctxDb.WithContext(ctx).Model(&Dlock{}).
+			Where("`key` = ? AND (expired_at < NOW() OR nonce = ?)", key, nonce).
+			Updates(map[string]interface{}{
+				"nonce":      nonce,
+				"expired_at": expAtGormExpr,
+				"updated_at": nowTsGormExpr,
+			})
+		return res.RowsAffected > 0, res.Error
+	} else { // Attempt to insert a new lock
+		res := ctxDb.Model(&Dlock{}).Create(map[string]interface{}{
 			"key":        key,
 			"nonce":      nonce,
 			"expired_at": expAtGormExpr,
 			"created_at": nowTsGormExpr,
 		})
-
-		// Insert successfully?
-		if res.Error == nil {
-			success = true
-			return nil
-		}
-
-		if isDuplicateKeyError(res.Error) {
-			// Lock exists, try to update if expired or nonce matches.
-			res = tx.Model(&Dlock{}).
-				Where("`key` = ? AND (expired_at < NOW() OR nonce = ?)", key, nonce).
-				Updates(map[string]interface{}{
-					"nonce":      nonce,
-					"expired_at": expAtGormExpr,
-					"updated_at": nowTsGormExpr,
-				})
-
-			if res.Error == nil {
-				success = res.RowsAffected > 0
-				return nil
-			}
-		}
-
-		return res.Error
-	}); err != nil {
-		return false, err
+		return res.Error == nil, res.Error
 	}
-
-	return success, nil
 }
 
 // Implement DelEntry - release a lock
 func (m *MySQLBackend) DelEntry(ctx context.Context, key, nonce string) (bool, error) {
 	res := m.db.WithContext(ctx).Where("`key` = ? AND nonce = ?", key, nonce).Delete(&Dlock{})
 	return res.RowsAffected > 0, res.Error
-}
-
-// Helper function to check if an error is a duplicate key error
-func isDuplicateKeyError(err error) bool {
-	var mysqlErr *mysql.MySQLError
-	return errors.As(err, &mysqlErr) && mysqlErr.Number == 1062
 }
