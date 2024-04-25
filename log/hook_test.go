685fc0d590272ceab648327587e9cc080db367f5
Liqun <wanliqunn@163.com>
2024-04-25 19:11:39 +0800

    fix unit testing (#30)

--- log/hook_test.go
+++ log/hook_test.go
@@ -1,23 +1,35 @@
 package log
 
 import (
+	"os"
 	"testing"
 
 	"github.com/Conflux-Chain/go-conflux-util/alert"
 	"github.com/sirupsen/logrus"
 )
 
-func initTest() {
+// Please set the following enviroments before running tests:
+// `TEST_DINGTALK_WEBHOOK`: DingTalk webhook;
+// `TEST_DINGTALK_SECRET`: DingTalk secret.
+
+func TestMain(m *testing.M) {
+	webhook := os.Getenv("TEST_DINGTALK_WEBHOOK")
+	secret := os.Getenv("TEST_DINGTALK_SECRET")
+
+	if len(webhook) == 0 || len(secret) == 0 {
+		return
+	}
+
 	alert.InitDingTalk(&alert.DingTalkConfig{
 		Enabled: true,
-		Webhook: "http://test.webhook",
-		Secret:  "test.secret",
+		Webhook: webhook,
+		Secret:  secret,
 	}, []string{"log", "hook", "test"})
+
+	os.Exit(m.Run())
 }
 
 func TestLogrusAddHooks(t *testing.T) {
-	initTest()
-
 	// Add alert hook for logrus fatal/warn/error level
 	hookLevels := []logrus.Level{logrus.FatalLevel, logrus.WarnLevel, logrus.ErrorLevel}
 	dingTalkAlertHook := NewDingTalkAlertHook(hookLevels)
