df08d3fc02eeb63e205899cb9a5dac17d636ed5c
Conflux <wanliqunn@163.com>
2024-05-08 14:41:14 +0800

    fix comment

--- log/hook/async.go
+++ log/hook/async.go
@@ -4,68 +4,50 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"sync"
+	"os"
 	"sync/atomic"
 
 	"github.com/sirupsen/logrus"
 )
 
-type runningStatus uint32
-
-const (
-	statusStopped runningStatus = iota
-	statusStarted
-)
-
-var (
-	errBufferFull = errors.New("async hook buffer is full")
-	errNotRunning = errors.New("async hook is not yet running")
-)
-
 // Async hook configuration.
 type AsyncOption struct {
-	// The number of goroutines kept in a pool while idle.
-	IdlePoolWorkers uint32 `default:"4"`
-	// The max number of goroutines that can be running concurrently while busy.
-	MaxConcurrentWorkers uint32 `default:"16"`
+	// Enable async mode or not
+	Enabled bool
+	// The max number of worker goroutines that can be running concurrently.
+	MaxWorkers uint32 `default:"4"`
 	// The maximum number of jobs that can be queued before processing begins.
-	JobQueueSize uint32 `default:"128"`
+	QueueSize uint32 `default:"32"`
 }
 
-// AsyncWrapper is an async wrapper that uses goroutines to invoke the wrapped hook.
-type AsyncWrapper struct {
+// AsyncHook is an async wrapper that uses goroutines to invoke an wrapped hook.
+type AsyncHook struct {
 	logrus.Hook
 	AsyncOption
 
-	// The flag to indicate running status: 0 - stopped, 1 - running.
-	runningFlag uint32
 	// The number of currently running worker goroutines.
 	numWorkers uint32
 	// Job queue for log entries that will be handled by worker goroutines.
 	jobQueue chan *logrus.Entry
-	// Keeps tracking of worker goroutines.
-	workerTracker sync.WaitGroup
-
-	// The context and cancel function for life cycle control.
-	ctx    context.Context
-	cancel context.CancelFunc
+	// The context for life cycle control.
+	ctx context.Context
 }
 
-func NewAsyncWrapper(wrappedHook logrus.Hook, opts AsyncOption) *AsyncWrapper {
-	return &AsyncWrapper{
+func NewAsyncHook(ctx context.Context, hook logrus.Hook, opts AsyncOption) *AsyncHook {
+	h := &AsyncHook{
 		AsyncOption: opts,
-		Hook:        wrappedHook,
-		jobQueue:    make(chan *logrus.Entry, opts.JobQueueSize),
+		Hook:        hook,
+		ctx:         ctx,
+		jobQueue:    make(chan *logrus.Entry, opts.QueueSize),
 	}
+
+	h.start()
+	return h
 }
 
-func (w *AsyncWrapper) Fire(entry *logrus.Entry) error {
-	if !w.isRunning() {
-		return errNotRunning
-	}
-
+func (w *AsyncHook) Fire(entry *logrus.Entry) error {
 	if entry.Level <= logrus.FatalLevel {
-		// For fatal or panic level logging, we have to fire the wrapped hook
+		// For `fatal` or `panic` level logging, we have to fire the wrapped hook
 		// synchronously, otherwise the hook would never be triggered due to
 		// application life cycle.
 		return w.Hook.Fire(entry)
@@ -75,7 +57,7 @@ func (w *AsyncWrapper) Fire(entry *logrus.Entry) error {
 	case w.jobQueue <- entry:
 		return nil
 	case <-w.ctx.Done():
-		return w.ctx.Err()
+		return nil
 	default:
 		// job queue is full because workers are too busy or too slow
 		// try to boost the workers if possible.
@@ -83,66 +65,40 @@ func (w *AsyncWrapper) Fire(entry *logrus.Entry) error {
 	}
 }
 
-func (w *AsyncWrapper) Start(ctx context.Context) {
-	if w.isRunning() { // already started?
-		return
-	}
+func (h *AsyncHook) start() {
+	h.incrWorkerCounter()
 
-	w.ctx, w.cancel = context.WithCancel(ctx)
-	defer w.setRunningFlag(statusStarted)
-
-	for i := uint32(0); i < w.IdlePoolWorkers; i++ {
-		w.workerTracker.Add(1)
-		w.incrWorkerCounter()
-
-		go func() {
-			defer w.workerTracker.Done()
-			defer w.decrWorkerCounter()
-
-			for {
-				select {
-				case entry := <-w.jobQueue:
-					w.wrapCall(entry)
-				case <-w.ctx.Done():
-					return
-				}
+	go func() {
+		defer h.decrWorkerCounter()
+		for {
+			select {
+			case entry := <-h.jobQueue:
+				h.fire(entry)
+			case <-h.ctx.Done():
+				return
 			}
-		}()
-	}
-}
-
-func (w *AsyncWrapper) Stop() {
-	if !w.isRunning() { // not started?
-		return
-	}
-
-	w.setRunningFlag(statusStopped)
-
-	// cancel and wait for all workers to complete and exit
-	w.cancel()
-	w.workerTracker.Wait()
+		}
+	}()
 }
 
 // boost starts extra goroutine to help empty out the job queue.
-func (w *AsyncWrapper) boost(entry *logrus.Entry) error {
-	if w.incrWorkerCounter() > w.MaxConcurrentWorkers {
-		w.decrWorkerCounter()
-		return errBufferFull
+func (h *AsyncHook) boost(entry *logrus.Entry) error {
+	if h.incrWorkerCounter() > h.MaxWorkers {
+		h.decrWorkerCounter()
+		return errors.New("async hook buffer is full")
 	}
 
-	w.workerTracker.Add(1)
 	go func() {
-		defer w.workerTracker.Done()
-		defer w.decrWorkerCounter()
+		defer h.decrWorkerCounter()
 
-		enq := w.jobQueue
+		enq := h.jobQueue
 		for {
 			select {
-			case entry := <-w.jobQueue:
-				w.wrapCall(entry)
+			case entry := <-h.jobQueue:
+				h.fire(entry)
 			case enq <- entry:
 				enq, entry = nil, nil
-			case <-w.ctx.Done():
+			case <-h.ctx.Done():
 				return
 			default:
 				return
@@ -153,31 +109,21 @@ func (w *AsyncWrapper) boost(entry *logrus.Entry) error {
 	return nil
 }
 
-func (w *AsyncWrapper) wrapCall(entry *logrus.Entry) {
-	if err := w.Hook.Fire(entry); err != nil {
-		formatter := logrus.JSONFormatter{}
+func (h *AsyncHook) fire(entry *logrus.Entry) {
+	if err := h.Hook.Fire(entry); err != nil {
+		formatter := logrus.TextFormatter{}
 		entryStr, _ := formatter.Format(entry)
 
-		fmt.Printf("Asyn hook call error %v with entry %v\n", err, entryStr)
+		fmt.Fprintf(os.Stderr, "Asyn hook fire error %v with entry %v\n", err, entryStr)
 	}
 }
 
-// set running flag status
-func (w *AsyncWrapper) setRunningFlag(status runningStatus) {
-	atomic.StoreUint32(&w.runningFlag, uint32(status))
-}
-
-// check if the async hook is running or not
-func (w *AsyncWrapper) isRunning() bool {
-	return atomic.LoadUint32(&w.runningFlag) == uint32(statusStarted)
-}
-
 // decrement the number of job workers by 1
-func (w *AsyncWrapper) decrWorkerCounter() uint32 {
-	return atomic.AddUint32(&w.numWorkers, ^uint32(0))
+func (h *AsyncHook) decrWorkerCounter() uint32 {
+	return atomic.AddUint32(&h.numWorkers, ^uint32(0))
 }
 
 // increment the number of job workers by 1
-func (w *AsyncWrapper) incrWorkerCounter() uint32 {
-	return atomic.AddUint32(&w.numWorkers, 1)
+func (h *AsyncHook) incrWorkerCounter() uint32 {
+	return atomic.AddUint32(&h.numWorkers, 1)
 }
