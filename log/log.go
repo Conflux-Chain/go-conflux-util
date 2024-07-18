2d6d5a4d3cfe4192f614778128c5910654ec661d
Conflux <wanliqunn@163.com>
2024-07-18 15:12:15 +0800

    Upgrade `go-ethereum` and migrate metrics components
    - Bumped the `go-ethereum` dependency to version v1.14.5 in `go.mod`.
    - Updated geth logging adaptation for compatibility with the new version.
    - Migrated metrics components from `Confura` for reusability.
    - Ensured logging configuration and adaptation work seamlessly with the new version.
    - Verified changes with build and test commands to ensure stability.

--- log/log.go
+++ log/log.go
@@ -2,15 +2,14 @@ package log
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"runtime"
-	"strings"
 	"sync"
 
 	"github.com/Conflux-Chain/go-conflux-util/log/hook"
 	viperUtil "github.com/Conflux-Chain/go-conflux-util/viper"
 	"github.com/ethereum/go-ethereum/log"
+	slogrus "github.com/samber/slog-logrus/v2"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -105,41 +104,14 @@ func mustInit(conf LoggingConfig, ctx context.Context, wg *sync.WaitGroup) {
 
 // adaptGethLogger adapt geth logger to work with logrus.
 func adaptGethLogger() {
-	formatter := log.TerminalFormat(false)
+	// Map extra slogrus log levels from geth to logrus log levels.
+	slogrus.LogLevels[log.LevelCrit] = logrus.FatalLevel
+	slogrus.LogLevels[log.LevelTrace] = logrus.TraceLevel
+	// Geth warn logging is little bit verbose, adapt it to logrus info level.
+	slogrus.LogLevels[log.LevelWarn] = logrus.InfoLevel
 
-	// Mapping from geth log level to logrus level
-	logrusLevelsMap := map[log.Lvl]logrus.Level{
-		log.LvlCrit:  logrus.FatalLevel,
-		log.LvlError: logrus.ErrorLevel,
-		// Geth warn logging is little bit verbose,
-		// adapt it to logrus info level.
-		log.LvlWarn:  logrus.InfoLevel,
-		log.LvlInfo:  logrus.InfoLevel,
-		log.LvlDebug: logrus.DebugLevel,
-		log.LvlTrace: logrus.TraceLevel,
-	}
-
-	log.Root().SetHandler(log.FuncHandler(func(r *log.Record) error {
-		logLvl, ok := logrusLevelsMap[r.Lvl]
-		if !ok {
-			return errors.New("unsupported geth log level")
-		}
-
-		// only logging above mapped logrus level
-		if logLvl <= logrus.GetLevel() {
-			logStr := string(formatter.Format(r))
-			abbrStr := logStr
-
-			firstLineEnd := strings.IndexRune(logStr, '\n')
-			if firstLineEnd > 0 { // extract first line as abstract
-				abbrStr = logStr[:firstLineEnd]
-			}
-
-			logrus.WithField("gethWrappedLogs", logStr).Log(logLvl, abbrStr)
-		}
-
-		return nil
-	}))
+	slogrusHandler := slogrus.Option{}.NewLogrusHandler()
+	log.SetDefault(log.NewLogger(slogrusHandler.WithGroup("geth")))
 }
 
 // BindFlags binds logging relevant flags for specified command.
