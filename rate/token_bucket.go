f2b89a492b938bb0a1a131852e86246e0378ca7a
boqiu <82121246@qq.com>
2026-07-27 16:30:46 +0800

    fix comments

--- rate/token_bucket.go
+++ rate/token_bucket.go
@@ -59,8 +59,7 @@ func (bucket *TokenBucket) LimitAt(now time.Time, n int) error {
 }
 
 func (bucket *TokenBucket) Expired() bool {
-	lastSeen := atomic.LoadInt64(&bucket.lastSeen)
-	return time.Now().Unix() > lastSeen+bucket.timeoutSecs
+	return bucket.ExpiredAt(time.Now())
 }
 
 func (bucket *TokenBucket) ExpiredAt(now time.Time) bool {
